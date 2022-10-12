78a29058ec8e0b07eb2a9474e0c34f66a96a1f47
LakshmanJ24 <100009458+LakshmanJ24@users.noreply.github.com>
2022-10-12 21:52:39 +0530

    9th try

--- Main.py
+++ Main.py
@@ -3,23 +3,29 @@ from typing import List
 def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
   x = nums1[0:m]
   y = nums2[0:n]
-  nums1=x+y
+  nums1 = x + y
   nums1 = merge_sort(nums1)
   return nums1
+  
   
 def merge_sort(myList) -> None:
   if len(myList) > 1:
         mid = len(myList) // 2
         left = myList[:mid]
         right = myList[mid:]
+
         # Recursive call on each half
         merge_sort(left)
         merge_sort(right)
+
         # Two iterators for traversing the two halves
         i = 0
         j = 0
+        
         # Iterator for the main list
-        k = 0
+        k=0
+        
+        
         while i < len(left) and j < len(right):
             if left[i] <= right[j]:
               # The value from the left half has been used
@@ -31,11 +37,13 @@ def merge_sort(myList) -> None:
                 j += 1
             # Move to the next slot
             k += 1
+
         # For all the remaining values
         while i < len(left):
             myList[k] = left[i]
             i += 1
             k += 1
+
         while j < len(right):
             myList[k]=right[j]
             j += 1
